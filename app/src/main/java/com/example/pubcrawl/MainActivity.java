e6fd27db0e80d654c14e774ae2e6dbaf94cf7cd6
Alex Childers <42629593+abchilders@users.noreply.github.com>
2020-04-28 09:55:17 -0700

    WIP - event planner option in hamburger menu goes to event planner fragment

--- app/src/main/java/com/example/pubcrawl/MainActivity.java
+++ app/src/main/java/com/example/pubcrawl/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.pubcrawl;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
@@ -23,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
     private NavigationView nvDrawer;
     final FragmentManager fragmentManager = getSupportFragmentManager();
 
+    private ActionBarDrawerToggle drawerToggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,11 +42,70 @@ public class MainActivity extends AppCompatActivity {
 
         // Find drawer view
         mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawerToggle = setupDrawerToggle(); 
+        nvDrawer = (NavigationView) findViewById(R.id.nvView);
+
+        // Setup toggle to display hamburger icon with nice animation 
+        drawerToggle.setDrawerIndicatorEnabled(true);
+        drawerToggle.syncState();
+        
+        // Tie DrawerLayout events to the ActionBarToggle 
+        mDrawer.addDrawerListener(drawerToggle); 
+        
+        // Set up drawer view
+        setupDrawerContent(nvDrawer);
 
         // temporary hack just to display events fragment
         Fragment fragment = new EventsFragment();
         fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
         Log.i(TAG, "Created EventsFragment");
+    }
+
+    private ActionBarDrawerToggle setupDrawerToggle() {
+        return new ActionBarDrawerToggle(this, mDrawer, toolbar, R.string.drawer_open,  R.string.drawer_close);
+    }
+
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        selectDrawerItem(menuItem);
+                        return true;
+                    }
+                });
+    }
+
+    public void selectDrawerItem(MenuItem menuItem) {
+        // Create a new fragment and specify the fragment to show based on nav item clicked
+        Fragment fragment = null;
+        Class fragmentClass;
+        switch(menuItem.getItemId()) {
+            case R.id.nav_event_planner:
+                fragmentClass = EventsFragment.class;
+                break;
+            // TODO: add case for logout button
+            default:
+                fragmentClass = EventsFragment.class;
+        }
+
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Insert the fragment by replacing any existing fragment
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+
+        // Highlight the selected item has been done by NavigationView
+        menuItem.setChecked(true);
+        // Set action bar title
+        setTitle(menuItem.getTitle());
+        // Close the navigation drawer
+        mDrawer.closeDrawers();
     }
 
     @Override
